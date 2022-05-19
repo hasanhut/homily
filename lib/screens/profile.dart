91f09fadca5331ca1d83b98cb6e1783d21f4f90b
hasanhut <hasanhut34@gmail.com>
2022-05-19 18:19:53 +0300

    update

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:homily/screens/homepage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
@@ -14,9 +13,11 @@ class _ProfilePageState extends State<ProfilePage> {
   String userId = '';
   String username = '';
   String userEmail = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text('H O M I L Y'),
         backgroundColor: Colors.black,
@@ -29,32 +30,62 @@ class _ProfilePageState extends State<ProfilePage> {
             if (snapshot.connectionState != ConnectionState.done) {
               return Text("Loading data....");
             }
-            return Column(
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 50,
-                  //backgroundImage: ,
+            return Stack(
+              children: [
+                Container(
+                  color: Colors.white,
+                  height: double.infinity,
+                  width: double.infinity,
+                  padding: EdgeInsets.only(top: 250, left: 20, right: 20),
                 ),
-                Text(
-                  'Username : $username',
-                  style: TextStyle(
-                      fontSize: 25.0,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold),
+                Container(
+                  height: 180,
+                  color: Colors.white,
                 ),
-                Text(
-                  'Email : $userEmail',
-                  style: TextStyle(
-                      fontSize: 25.0,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  'Family Name : ------',
-                  style: TextStyle(
-                      fontSize: 25.0,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold),
+                Container(
+                  width: double.infinity,
+                  height: 190,
+                  margin: EdgeInsets.symmetric(vertical: 40, horizontal: 20),
+                  decoration: BoxDecoration(
+                    color: Color(0xfff6f6f6f),
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Color.fromARGB(90, 20, 20, 20),
+                          blurRadius: 0.8,
+                          offset: Offset(9, 9))
+                    ],
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 45,
+                        child: CircleAvatar(
+                          backgroundColor: Colors.grey,
+                          radius: 42, //backgroundImage: ,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Text(
+                        '$username',
+                        style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 2,
+                      ),
+                      Text(
+                        '$userEmail',
+                        style: TextStyle(color: Colors.grey[800]),
+                      ),
+                    ],
+                  ),
                 )
               ],
             );
